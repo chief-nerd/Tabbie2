ed1eedff4a404c6f2cffacc464aa4512f9f2e3e9
Jakob Reiter <jakob.reiter.io@gmail.com>
2014-11-15 17:16:41 +0100

    Change Name back ;)

--- tabbie2.git/common/config/params.php
+++ tabbie2.git/common/config/params.php
@@ -4,7 +4,7 @@ return [
     'adminEmail' => 'admin@tabbie.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
-    'appName' => 'TabB',
+    'appName' => 'Tabbie2',
     'slogan' => 'BP Tabbing so easy your mother can do it',
     'algoOptions' => [ //Installed Algorythemen Options
         "DummyTest"
