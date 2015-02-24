e78c6d21b1e1bf4571840a6039e6987f5456c493
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-02-24 12:25:10 +0100

    Run basic fixture generate

--- tabbie2.git/console/config/main.php
+++ tabbie2.git/console/config/main.php
@@ -1,9 +1,9 @@
 <?php
+
+Yii::setAlias("tests", dirname(dirname(__DIR__)) . "/tests/codeception/common/");
+
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+        require(__DIR__ . '/../../common/config/params.php'), require(__DIR__ . '/../../common/config/params-local.php'), require(__DIR__ . '/params.php'), require(__DIR__ . '/params-local.php')
 );
 
 return [
@@ -11,6 +11,13 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log', 'gii'],
     'controllerNamespace' => 'console\controllers',
+    'controllerMap' => [
+        'fixture' => [
+            'class' => 'yii\faker\FixtureController',
+            'templatePath' => '@tests/templates/fixtures',
+            'count' => 10,
+        ],
+    ],
     'modules' => [
         'gii' => 'yii\gii\Module',
     ],
