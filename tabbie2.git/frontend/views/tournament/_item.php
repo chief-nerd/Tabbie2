b98cae83aa81679feaef70a6dd37c2eefcab5947
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-03-10 14:39:37 +0100

    Remove Button Enter Result from this site

--- tabbie2.git/frontend/views/tournament/_item.php
+++ tabbie2.git/frontend/views/tournament/_item.php
@@ -7,20 +7,8 @@ use yii\helpers\Url;
     <div class="col-sm-2">
         <?= Html::img($tournament->logo, ["style" => "height:80px; margin: 10px;"]) ?>
     </div>
-    <div class="col-sm-6">
+    <div class="col-sm-10">
         <h2><?= Html::encode($tournament->name) ?></h2>
         <h4><?= Html::encode($tournament->start_date) ?> - <?= Html::encode($tournament->end_date) ?></h4>
-    </div>
-    <div class="col-sm-4 form-group">
-        <?
-        $debate_id = $this->context->activeInputAvailable($tournament);
-        if (isset($debate_id) && $debate_id != false) {
-            echo Html::a(Yii::t('app', 'Enter Results'), [
-                'result/create',
-                'tournament_id' => $tournament->id,
-                'id' => $debate_id
-                    ], ['class' => 'btn btn-lg btn-success']);
-        }
-        ?>
     </div>
 </a>
