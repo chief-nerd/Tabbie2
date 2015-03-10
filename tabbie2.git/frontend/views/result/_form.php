5ef59f38c578ed9b10710a9c0e2abdcd21930193
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-03-10 14:42:55 +0100

    Add Team names

--- tabbie2.git/frontend/views/result/_form.php
+++ tabbie2.git/frontend/views/result/_form.php
@@ -26,11 +26,13 @@ use yii\widgets\ActiveForm;
     <div class="row">
         <div class="<?= $cols ?>">
             <h3>Opening Government</h3>
+            <h4><?= $debate->og_team->name ?></h4>
             <?= $form->field($model, 'og_A_speaks', $fieldOption)->label($debate->og_team->speakerA->name)->textInput($textOption) ?>
             <?= $form->field($model, 'og_B_speaks', $fieldOption)->label($debate->og_team->speakerB->name)->textInput($textOption) ?>
         </div>
         <div class="<?= $cols ?>">
             <h3>Opening Opposition</h3>
+            <h4><?= $debate->oo_team->name ?></h4>
             <?= $form->field($model, 'oo_A_speaks', $fieldOption)->label($debate->oo_team->speakerA->name)->textInput($textOption) ?>
             <?= $form->field($model, 'oo_B_speaks', $fieldOption)->label($debate->oo_team->speakerB->name)->textInput($textOption) ?>
         </div>
@@ -38,11 +40,13 @@ use yii\widgets\ActiveForm;
     <div class="row">
         <div class="<?= $cols ?>">
             <h3>Closing Government</h3>
+            <h4><?= $debate->cg_team->name ?></h4>
             <?= $form->field($model, 'cg_A_speaks', $fieldOption)->label($debate->cg_team->speakerA->name)->textInput($textOption) ?>
             <?= $form->field($model, 'cg_B_speaks', $fieldOption)->label($debate->cg_team->speakerB->name)->textInput($textOption) ?>
         </div>
         <div class="<?= $cols ?>">
             <h3>Closing Opposition</h3>
+            <h4><?= $debate->co_team->name ?></h4>
             <?= $form->field($model, 'co_A_speaks', $fieldOption)->label($debate->co_team->speakerA->name)->textInput($textOption) ?>
             <?= $form->field($model, 'co_B_speaks', $fieldOption)->label($debate->co_team->speakerB->name)->textInput($textOption) ?>
         </div>
