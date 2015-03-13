66c6a5ab84a2c33383ffe83a4154e016e9c2d4a5
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-03-13 15:52:12 +0100

    Reformatting and Logo Placement

--- tabbie2.git/frontend/controllers/TournamentController.php
+++ tabbie2.git/frontend/controllers/TournamentController.php
@@ -16,185 +16,146 @@ use \common\models;
  */
 class TournamentController extends BaseController {
 
-    public function behaviors() {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'actions' => ['index', 'view', 'display'],
-                        'roles' => [],
-                    ],
-                    [
-                        'allow' => true,
-                        'actions' => ['create'],
-                        'roles' => ['@'],
-                    ],
-                    [
-                        'allow' => true,
-                        'actions' => ['update'],
-                        'matchCallback' => function ($rule, $action) {
-                    return (Yii::$app->user->isTabMaster($this->_tournament) || Yii::$app->user->isConvenor($this->_tournament));
-                }
-                    ],
-                ],
-            ],
-            'tournamentFilter' => [
-                'class' => TournamentContextFilter::className(),
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['post'],
-                ],
-            ],
-        ];
-    }
+	public function behaviors() {
+		return ['access' => ['class' => AccessControl::className(), 'rules' => [['allow' => true, 'actions' => ['index', 'view', 'display'], 'roles' => [],], ['allow' => true, 'actions' => ['create'], 'roles' => ['@'],], ['allow' => true, 'actions' => ['update'], 'matchCallback' => function ($rule, $action) {
+			return (Yii::$app->user->isTabMaster($this->_tournament) || Yii::$app->user->isConvenor($this->_tournament));
+		}],],], 'tournamentFilter' => ['class' => TournamentContextFilter::className(),], 'verbs' => ['class' => VerbFilter::className(), 'actions' => ['delete' => ['post'],],],];
+	}
 
-    /**
-     * Lists all Tournament models.
-     * @return mixed
-     */
-    public function actionIndex() {
-        $searchModel = new TournamentSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+	/**
+	 * Lists all Tournament models.
+	 * @return mixed
+	 */
+	public function actionIndex() {
+		$searchModel = new TournamentSearch();
+		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
 
-        return $this->render('index', [
-                    'searchModel' => $searchModel,
-                    'dataProvider' => $dataProvider,
-        ]);
-    }
+		return $this->render('index', ['searchModel' => $searchModel, 'dataProvider' => $dataProvider,]);
+	}
 
-    /**
-     * Displays a current Tournament model.
-     * @return mixed
-     */
-    public function actionView($id) {
-        return $this->render('view', [
-                    'model' => $this->findModel($id),
-        ]);
-    }
+	/**
+	 * Displays a current Tournament model.
+	 * @return mixed
+	 */
+	public function actionView($id) {
+		return $this->render('view', ['model' => $this->findModel($id),]);
+	}
 
-    /**
-     * Creates a new Tournament model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate() {
-        $model = new Tournament();
+	/**
+	 * Creates a new Tournament model.
+	 * If creation is successful, the browser will be redirected to the 'view' page.
+	 * @return mixed
+	 */
+	public function actionCreate() {
+		$model = new Tournament();
 
-        if (Yii::$app->request->isPost) {
-            $file = \yii\web\UploadedFile::getInstance($model, 'logo');
-            $model->load(Yii::$app->request->post());
-            $model->generateUrlSlug();
-            $path = "/uploads/TournamentLogo-" . $model->url_slug . ".jpg";
-            if ($file && $file->saveAs(Yii::getAlias("@frontend/web") . $path))
-                $model->logo = $path;
-            else
-                $model->logo = null;
+		if (Yii::$app->request->isPost) {
+			$file = \yii\web\UploadedFile::getInstance($model, 'logo');
+			$model->load(Yii::$app->request->post());
+			$model->generateUrlSlug();
+			$path = "/uploads/TournamentLogo-" . $model->url_slug . ".jpg";
+			$model->logo = $file && $file->saveAs(Yii::getAlias("@frontend/web") . $path) ? $path : null;
 
-            if ($model->save()) {
-                $energyConf = new models\EnergyConfig();
-                $energyConf->setup($model);
+			if ($model->save()) {
+				$energyConf = new models\EnergyConfig();
+				$energyConf->setup($model);
 
-                return $this->redirect(['view', 'id' => $model->id]);
-            } else {
-                Yii::$app->session->setFlash("error", "Can't save Tournament!" . print_r($model->getErrors(), true));
-            }
-        }
-        return $this->render('create', [
-                    'model' => $model,
-        ]);
-    }
+				return $this->redirect(['view', 'id' => $model->id]);
+			}
+			else {
+				Yii::$app->session->setFlash("error", "Can't save Tournament!" . print_r($model->getErrors(), true));
+			}
+		}
+		return $this->render('create', ['model' => $model,]);
+	}
 
-    /**
-     * Updates an existing Tournament model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id) {
-        $model = $this->findModel($id);
+	/**
+	 * Updates an existing Tournament model.
+	 * If update is successful, the browser will be redirected to the 'view' page.
+	 *
+	 * @param integer $id
+	 *
+	 * @return mixed
+	 */
+	public function actionUpdate($id) {
+		$model = $this->findModel($id);
 
-        if (Yii::$app->request->isPost) {
+		if (Yii::$app->request->isPost) {
 
-            //Upload File
-            $file = \yii\web\UploadedFile::getInstance($model, 'logo');
+			//Upload File
+			$file = \yii\web\UploadedFile::getInstance($model, 'logo');
 
-            //Save Old File Path
-            $oldFile = $model->logo;
-            //Load new values
-            $model->load(Yii::$app->request->post());
-            $path = "/uploads/TournamentLogo-" . $model->url_slug . ".jpg";
+			//Save Old File Path
+			$oldFile = $model->logo;
+			//Load new values
+			$model->load(Yii::$app->request->post());
+			$path = "/uploads/TournamentLogo-" . $model->url_slug . ".jpg";
 
-            if ($file !== NULL) {
-                //Save new File
-                if ($file->saveAs(Yii::getAlias("@frontend/web") . $path))
-                    $model->logo = $path;
-            } else
-                $model->logo = $oldFile;
+			if ($file !== null) {
+				//Save new File
+				if ($file->saveAs(Yii::getAlias("@frontend/web") . $path)) $model->logo = $path;
+			}
+			else
+				$model->logo = $oldFile;
 
-            if ($model->save()) {
-                return $this->redirect(['view', 'id' => $model->id]);
-            }
-        }
+			if ($model->save()) {
+				return $this->redirect(['view', 'id' => $model->id]);
+			}
+		}
 
-        return $this->render('update', [
-                    'model' => $model,
-        ]);
-    }
+		return $this->render('update', ['model' => $model,]);
+	}
 
-    /**
-     * Deletes an existing Tournament model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id) {
-        $this->findModel($id)->delete();
+	/**
+	 * Deletes an existing Tournament model.
+	 * If deletion is successful, the browser will be redirected to the 'index' page.
+	 *
+	 * @param integer $id
+	 *
+	 * @return mixed
+	 */
+	public function actionDelete($id) {
+		$this->findModel($id)->delete();
 
-        return $this->redirect(['index']);
-    }
+		return $this->redirect(['index']);
+	}
 
-    /**
-     * Finds the Tournament model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Tournament the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id) {
-        if (($model = Tournament::findOne(["id" => $id])) !== null) {
-            return $model;
-        } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
-        }
-    }
+	/**
+	 * Finds the Tournament model based on its primary key value.
+	 * If the model is not found, a 404 HTTP exception will be thrown.
+	 *
+	 * @param integer $id
+	 *
+	 * @return Tournament the loaded model
+	 * @throws NotFoundHttpException if the model cannot be found
+	 */
+	protected function findModel($id) {
+		if (($model = Tournament::findOne(["id" => $id])) !== null) {
+			return $model;
+		}
+		else {
+			throw new NotFoundHttpException('The requested page does not exist.');
+		}
+	}
 
-    /**
-     *
-     * @param Tournament $tournament
-     * @return int|false
-     */
-    public function activeInputAvailable($tournament) {
-        $user_id = Yii::$app->user->id;
+	/**
+	 *
+	 * @param Tournament $tournament
+	 *
+	 * @return int|false
+	 */
+	public function activeInputAvailable($tournament) {
+		$user_id = Yii::$app->user->id;
 
-        $activeRound = models\Round::findOne([
-                    "tournament_id" => $tournament->id,
-                    "displayed" => 1,
-                    "published" => 1,
-                    "closed" => 0,
-        ]);
+		$activeRound = models\Round::findOne(["tournament_id" => $tournament->id, "displayed" => 1, "published" => 1, "closed" => 0,]);
 
-        if ($activeRound) {
-            $debate = models\Debate::findOneByChair($user_id, $tournament->id, $activeRound->id);
-            if ($debate instanceof models\Debate)
-                return $debate->id;
-        }
+		if ($activeRound) {
+			$debate = models\Debate::findOneByChair($user_id, $tournament->id, $activeRound->id);
+			if ($debate instanceof models\Debate) return $debate->id;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
 }
--- tabbie2.git/frontend/views/layouts/main.php
+++ tabbie2.git/frontend/views/layouts/main.php
@@ -1,187 +1,184 @@
 <?php
 
-use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
-use yii\widgets\Breadcrumbs;
-use frontend\assets\AppAsset;
-use frontend\widgets\Alert;
-use common\models\Tournament;
+	use yii\helpers\Html;
+	use yii\bootstrap\Nav;
+	use yii\bootstrap\NavBar;
+	use yii\widgets\Breadcrumbs;
+	use frontend\assets\AppAsset;
+	use frontend\widgets\Alert;
+	use common\models\Tournament;
 
-/* @var $this \yii\web\View */
-/* @var $content string */
+	/* @var $this \yii\web\View */
+	/* @var $content string */
 
-AppAsset::register($this);
+	AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <?
-if ($this->context->hasMethod("_getContext")) {
-    $tournament = $this->context->_getContext();
-    if ($tournament instanceof Tournament && (Yii::$app->user->isTabMaster($tournament) || Yii::$app->user->isAdmin())) {
-        $addclass = "movedown";
-    }
-}
+	if ($this->context->hasMethod("_getContext")) {
+		$tournament = $this->context->_getContext();
+		if ($tournament instanceof Tournament && (Yii::$app->user->isTabMaster($tournament) || Yii::$app->user->isAdmin())) {
+			$addclass = "movedown";
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-    <head>
-        <meta charset="<?= Yii::$app->charset ?>"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<meta charset="<?= Yii::$app->charset ?>"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <?
-        if ($tournament instanceof Tournament) {
-            echo '<link rel = "apple-touch-icon" href = "' . $tournament->icon . '"/>';
-        }
-        ?>
+	<?
+		if ($tournament instanceof Tournament) {
+			echo '<link rel = "apple-touch-icon" href = "' . $tournament->logo . '"/>';
+		}
+	?>
 
-        <?= Html::csrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?> :: <?= Html::encode(Yii::$app->params["appName"]) ?></title>
-        <?php $this->head() ?>
-    </head>
-    <body class="<?= isset($addclass) ? $addclass : "" ?>">
-        <?php $this->beginBody() ?>
-        <div class="flashes">
-            <?= Alert::widget() ?>
-        </div>
-        <div class="wrap">
-            <?php
-            NavBar::begin([
-                'brandLabel' => Yii::$app->params["appName"],
-                'brandUrl' => Yii::$app->homeUrl,
-                'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
-                ],
-            ]);
-            $menuItems = [
-                ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Tournaments', 'url' => ['tournament/index']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
-            ];
-            if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-            } else {
-                $menuItems[] = [
-                    'label' => Yii::$app->user->getModel()->name . "'s Profile",
-                    'url' => ['user/view', 'id' => Yii::$app->user->id],
-                ];
-                $menuItems[] = [
-                    'label' => 'Logout',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ];
-            }
-            echo Nav::widget([
-                'options' => ['class' => 'navbar-nav menu navbar-right'],
-                'items' => $menuItems,
-            ]);
-            NavBar::end();
-            ?>
-            <?
-            /* @var $tournament common\models\Tournament */
-            if ($this->context->hasMethod("_getContext")) {
-                $tournament = $this->context->_getContext();
-                if ($tournament instanceof \common\models\Tournament && (Yii::$app->user->isTabMaster($tournament) || Yii::$app->user->isAdmin())) {
+	<?= Html::csrfMetaTags() ?>
+	<title><?= Html::encode($this->title) ?> :: <?= Html::encode(Yii::$app->params["appName"]) ?></title>
+	<?php $this->head() ?>
+</head>
+<body class="<?= isset($addclass) ? $addclass : "" ?>">
+<?php $this->beginBody() ?>
+<div class="flashes">
+	<?= Alert::widget() ?>
+</div>
+<div class="wrap">
+	<?php
+		NavBar::begin([
+			'brandLabel' => Yii::$app->params["appName"],
+			'brandUrl' => Yii::$app->homeUrl,
+			'options' => [
+				'class' => 'navbar-inverse navbar-fixed-top',
+			],
+		]);
+		$menuItems = [
+			['label' => 'Home', 'url' => ['/site/index']],
+			['label' => 'About', 'url' => ['/site/about']],
+			['label' => 'Tournaments', 'url' => ['tournament/index']],
+			['label' => 'Contact', 'url' => ['/site/contact']],
+		];
+		if (Yii::$app->user->isGuest) {
+			$menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
+			$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+		}
+		else {
+			$menuItems[] = [
+				'label' => Yii::$app->user->getModel()->name . "'s Profile",
+				'url' => ['user/view', 'id' => Yii::$app->user->id],
+			];
+			$menuItems[] = [
+				'label' => 'Logout',
+				'url' => ['/site/logout'],
+				'linkOptions' => ['data-method' => 'post']
+			];
+		}
+		echo Nav::widget([
+			'options' => ['class' => 'navbar-nav menu navbar-right'],
+			'items' => $menuItems,
+		]);
+		NavBar::end();
+	?>
+	<?
+		/* @var $tournament common\models\Tournament */
+		if ($this->context->hasMethod("_getContext")) {
+			$tournament = $this->context->_getContext();
+			if ($tournament instanceof \common\models\Tournament && (Yii::$app->user->isTabMaster($tournament) || Yii::$app->user->isAdmin())) {
 
-                    NavBar::begin([
-                        'brandLabel' => $tournament->name . " Tabmaster",
-                        'brandUrl' => Yii::$app->urlManager->createUrl(["tournament/view", "id" => $tournament->id]),
-                        'options' => [
-                            'class' => 'navbar navbar-default navbar-fixed-top tabmaster',
-                        ],
-                    ]);
+				NavBar::begin([
+					'brandLabel' => $tournament->name . " Tabmaster",
+					'brandUrl' => Yii::$app->urlManager->createUrl(["tournament/view", "id" => $tournament->id]),
+					'options' => [
+						'class' => 'navbar navbar-default navbar-fixed-top tabmaster',
+					],
+				]);
 
-                    $rounds = array();
-                    foreach ($tournament->rounds as $r) {
-                        $rounds[] = ['label' => "Round #$r->number", 'url' => ['round/view', "id" => $r->id, "tournament_id" => $tournament->id]];
-                        $results[] = ['label' => "Result Round #$r->number", 'url' => ['result/round', "id" => $r->id, "tournament_id" => $tournament->id]];
-                    }
-                    $menuItems = [
-                        ['label' => 'Venues', 'url' => '#',
-                            "items" => [
-                                ['label' => 'List Venues', 'url' => ['venue/index', "tournament_id" => $tournament->id]],
-                                ['label' => 'Create Venue', 'url' => ['venue/create', "tournament_id" => $tournament->id]],
-                            ]
-                        ],
-                        ['label' => 'Teams', 'url' => '#',
-                            "items" => [
-                                ['label' => 'List Teams', 'url' => ['team/index', "tournament_id" => $tournament->id]],
-                                ['label' => 'Create Team', 'url' => ['team/create', "tournament_id" => $tournament->id]],
-                                ['label' => 'Import Team', 'url' => ['team/import', "tournament_id" => $tournament->id]],
-                            ]
-                        ],
-                        ['label' => 'Adjudicators', 'url' => '#',
-                            "items" => [
-                                ['label' => 'List Adjudicators', 'url' => ['adjudicator/index', "tournament_id" => $tournament->id]],
-                                ['label' => 'Create Adjudicator', 'url' => ['adjudicator/create', "tournament_id" => $tournament->id]],
-                                ['label' => 'Import Adjudicator', 'url' => ['adjudicator/import', "tournament_id" => $tournament->id]],
-                                '<li class="divider"></li>',
-                                ['label' => 'Preset Adj. Panels', 'url' => ["panel/create", "tournament_id" => $tournament->id]],
-                            ]
-                        ],
-                        ['label' => 'Rounds', 'url' => '#',
-                            "items" => array_merge_recursive([
-                                ['label' => 'Energy Options', 'url' => ['energy/index', "tournament_id" => $tournament->id]],
-                                '<li class="divider"></li>',
-                                ['label' => 'List Rounds', 'url' => ['round/index', "tournament_id" => $tournament->id]],
-                                ['label' => 'Create Round', 'url' => ['round/create', "tournament_id" => $tournament->id]],
-                                '<li class="divider"></li>',
-                                    ], $rounds),
-                        ],
-                        ['label' => 'Results', 'url' => '#',
-                            "items" => array_merge_recursive([
-                                ['label' => 'List Results', 'url' => ['result/index', "tournament_id" => $tournament->id]],
-                                '<li class="divider"></li>',
-                                    ], $results),
-                        ],
-                        ['label' => 'Current Tab', 'url' => "#",
-                            "items" => [
-                                ['label' => 'Team Tab', 'url' => ['tab/team', "tournament_id" => $tournament->id]],
-                                ['label' => 'Speaker Tab', 'url' => ['tab/speaker', "tournament_id" => $tournament->id]]
-                            ]
-                        ],
-                        ['label' => 'Feedback', 'url' => '#',
-                            "items" => [
-                                ['label' => 'Every Feedback', 'url' => ['feedback/index', "tournament_id" => $tournament->id]],
-                                '<li class="divider"></li>',
-                                ['label' => 'Adjudicator Feedback', 'url' => ['feedback/adjudicator', "tournament_id" => $tournament->id]],
-                                ['label' => 'Team to Chair Feedback', 'url' => ['feedback/team', "tournament_id" => $tournament->id]],
-                                '<li class="divider"></li>',
-                                ['label' => 'Setup Questions', 'url' => ['question/index', "tournament_id" => $tournament->id]],
-                                '<li class="divider"></li>',
-                                ['label' => 'Tournament Feedback', 'url' => ['feedback/tournament', "tournament_id" => $tournament->id]],
-                                ['label' => Yii::$app->params["appName"] . ' Feedback', 'url' => ['feedback/tabbie', "tournament_id" => $tournament->id]],
-                            ]
-                        ],
-                    ];
+				$rounds = array();
+				foreach ($tournament->rounds as $r) {
+					$rounds[] = ['label' => "Round #$r->number", 'url' => ['round/view', "id" => $r->id, "tournament_id" => $tournament->id]];
+					$results[] = ['label' => "Result Round #$r->number", 'url' => ['result/round', "id" => $r->id, "tournament_id" => $tournament->id]];
+				}
+				$menuItems = [
+					['label' => 'Venues', 'url' => '#',
+						"items" => [
+							['label' => 'List Venues', 'url' => ['venue/index', "tournament_id" => $tournament->id]],
+							['label' => 'Create Venue', 'url' => ['venue/create', "tournament_id" => $tournament->id]],
+						]
+					],
+					['label' => 'Teams', 'url' => '#',
+						"items" => [
+							['label' => 'List Teams', 'url' => ['team/index', "tournament_id" => $tournament->id]],
+							['label' => 'Create Team', 'url' => ['team/create', "tournament_id" => $tournament->id]],
+							['label' => 'Import Team', 'url' => ['team/import', "tournament_id" => $tournament->id]],
+						]
+					],
+					['label' => 'Adjudicators', 'url' => '#',
+						"items" => [
+							['label' => 'List Adjudicators', 'url' => ['adjudicator/index', "tournament_id" => $tournament->id]],
+							['label' => 'Create Adjudicator', 'url' => ['adjudicator/create', "tournament_id" => $tournament->id]],
+							['label' => 'Import Adjudicator', 'url' => ['adjudicator/import', "tournament_id" => $tournament->id]],
+							'<li class="divider"></li>',
+							['label' => 'Preset Adj. Panels', 'url' => ["panel/create", "tournament_id" => $tournament->id]],
+						]
+					],
+					['label' => 'Rounds', 'url' => '#',
+						"items" => array_merge_recursive([
+							['label' => 'Energy Options', 'url' => ['energy/index', "tournament_id" => $tournament->id]],
+							'<li class="divider"></li>',
+							['label' => 'List Rounds', 'url' => ['round/index', "tournament_id" => $tournament->id]],
+							['label' => 'Create Round', 'url' => ['round/create', "tournament_id" => $tournament->id]],
+							'<li class="divider"></li>',
+						], $rounds),
+					],
+					['label' => 'Results', 'url' => '#',
+						"items" => array_merge_recursive([
+							['label' => 'List Results', 'url' => ['result/index', "tournament_id" => $tournament->id]],
+							'<li class="divider"></li>',
+						], $results),
+					],
+					['label' => 'Current Tab', 'url' => "#",
+						"items" => [
+							['label' => 'Team Tab', 'url' => ['tab/team', "tournament_id" => $tournament->id]],
+							['label' => 'Speaker Tab', 'url' => ['tab/speaker', "tournament_id" => $tournament->id]]
+						]
+					],
+					['label' => 'Feedback', 'url' => '#',
+						"items" => [
+							['label' => 'Every Feedback', 'url' => ['feedback/index', "tournament_id" => $tournament->id]],
+							'<li class="divider"></li>',
+							['label' => 'Adjudicator Feedback', 'url' => ['feedback/adjudicator', "tournament_id" => $tournament->id]],
+							['label' => 'Team to Chair Feedback', 'url' => ['feedback/team', "tournament_id" => $tournament->id]],
+							'<li class="divider"></li>',
+							['label' => 'Setup Questions', 'url' => ['question/index', "tournament_id" => $tournament->id]],
+							'<li class="divider"></li>',
+							['label' => 'Tournament Feedback', 'url' => ['feedback/tournament', "tournament_id" => $tournament->id]],
+							['label' => Yii::$app->params["appName"] . ' Feedback', 'url' => ['feedback/tabbie', "tournament_id" => $tournament->id]],
+						]
+					],
+				];
 
-                    echo Nav::widget([
-                        'options' => ['class' => 'navbar-nav navbar-right'],
-                        'items' => $menuItems,
-                    ]);
-                    NavBar::end();
-                }
-            }
-            ?>
-            <div class="container">
-                <?=
-                Breadcrumbs::widget([
-                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                ])
-                ?>
-                <?= $content ?>
-            </div>
+				echo Nav::widget(['options' => ['class' => 'navbar-nav navbar-right'], 'items' => $menuItems,]);
+				NavBar::end();
+			}
+		}
+	?>
+	<div class="container">
+		<?=
+			Breadcrumbs::widget(['links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],])
+		?>
+		<?= $content ?>
+	</div>
 
-            <footer class="footer">
-                <div class="container">
-                    <p class="pull-left"><?= Yii::$app->params["appName"] ?> &copy; <?= date('Y') ?></p>
-                    <p class="pull-right"><?= HTML::a("Contact", 'mailto:' . Yii::$app->params["adminEmail"]) ?></p>
-                </div>
-            </footer>
-        </div>
+	<footer class="footer">
+		<div class="container">
+			<p class="pull-left"><?= Yii::$app->params["appName"] ?> &copy; <?= date('Y') ?></p>
 
-        <?php $this->endBody() ?>
-    </body>
+			<p class="pull-right"><?= HTML::a("Contact", 'mailto:' . Yii::$app->params["adminEmail"]) ?></p>
+		</div>
+	</footer>
+</div>
+
+<?php $this->endBody() ?>
+</body>
 </html>
 <?php $this->endPage() ?>
