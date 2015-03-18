daafd7be224da794daa7456da03d0ca038e9073a
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-03-18 18:40:28 +0100

    RoundView Assets

---
+++ tabbie2.git/frontend/assets/RoundviewAsset.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @link      http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license   http://www.yiiframework.com/license/
+ */
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Round View Asset Group
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since  2.0
+ */
+class RoundviewAsset extends AssetBundle {
+
+	public $sourcePath = '@frontend/assets';
+	public $css = [
+		'css/round.css',
+		'css/color_pattern.css',
+	];
+	public $js = [
+		'js/adjudicatorActions.js',
+	];
+	public $depends = [
+		'frontend\assets\AppAsset',
+		'yii\web\YiiAsset',
+		'yii\bootstrap\BootstrapAsset',
+	];
+}
