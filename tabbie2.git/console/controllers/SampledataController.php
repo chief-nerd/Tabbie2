0773749d10c358a865a0b7645a9409c8330bff99
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-05-01 22:40:41 +0200

    SampleData generator

---
+++ tabbie2.git/console/controllers/SampledataController.php
@@ -0,0 +1,105 @@
+<?php
+namespace console\controllers;
+
+use Faker;
+use Yii;
+/**
+ * Created by PhpStorm.
+ * User: jareiter
+ * Date: 29/04/15
+ * Time: 18:11
+ */
+use yii\console\Controller;
+
+class SampledataController extends Controller {
+
+
+	public function actionGenerate($amount_teams, $amount_adj = null, $amount_venues = null) {
+
+		if (!is_numeric($amount_teams)) {
+			echo "Parameter Teams must be an integer";
+			exit();
+		}
+
+		if (!is_int($amount_venues)) {
+			$amount_venues = $amount_teams / 4;
+		}
+
+		if (!is_int($amount_adj)) {
+			$amount_adj = $amount_venues * 3;
+		}
+
+		echo "Run with:\n";
+		echo "Venues:\t" . $amount_venues . "\n";
+		echo "Teams:\t" . $amount_teams . "\n";
+		echo "Judges:\t" . $amount_adj . "\n";
+
+// use the factory to create a Faker\Generator instance
+		$faker = Faker\Factory::create();
+
+		$output_venue = "Name;Active\n";
+		$output_teams = "Team Name;Society Name;A.Givenname;A.Surename;A.Email;B.Givenname;B.Surename;B.Email\n";
+		$output_adjudicator = "Society;Givenname;Surename;Email;Strength\n";
+		$save = [];
+
+		echo "\nVenues:";
+		for ($v = 0; $v < $amount_venues; $v++) {
+			$line = "Room " . ($v + 1) . ";1\n";
+			//echo $line;
+			$output_venue .= $line;
+			echo ".";
+		}
+		echo "\tgenerated\n";
+
+		echo "Teams:";
+		for ($t = 0; $t < $amount_teams; $t++) {
+			$city = $faker->city;
+			$save[] = $city;
+			$line = $city . " " . strtoupper($faker->randomLetter) . ";" .
+				$city . " Debating Society;" .
+				addslashes($faker->firstName) . ";" .
+				addslashes($faker->lastName) . ";" .
+				$faker->email . ";" .
+				addslashes($faker->firstName) . ";" .
+				addslashes($faker->lastName) . ";" .
+				$faker->email . "\n";
+			//echo $line;
+			$output_teams .= $line;
+			echo ".";
+		}
+		echo "\tgenerated\n";
+		echo "Adjudicator:";
+		for ($a = 0; $a < $amount_adj; $a++) {
+
+			if ($faker->numberBetween(0, 100) > 40)
+				$society = $save[$faker->numberBetween(0, count($save) - 1)] . " Debating Society;";
+			else
+				$society = $faker->city . " Debating Society;";
+
+			$line = $society .
+				addslashes($faker->firstName) . ";" .
+				addslashes($faker->lastName) . ";" .
+				$faker->email . ";" .
+				$faker->numberBetween(0, 9) . "\n";
+			//echo $line;
+			$output_adjudicator .= $line;
+			echo ".";
+		}
+		echo "\tgenerated\n";
+
+		$base = Yii::getAlias("@frontend/assets/csv/");
+		$handle_venue = fopen($base . "Sample_Venues.csv", "w");
+		$handle_teams = fopen($base . "Sample_Teams.csv", "w");
+		$handle_adj = fopen($base . "Sample_Adjudicators.csv", "w");
+
+		fwrite($handle_venue, $output_venue);
+		fclose($handle_venue);
+
+		fwrite($handle_teams, $output_teams);
+		fclose($handle_teams);
+
+		fwrite($handle_adj, $output_adjudicator);
+		fclose($handle_adj);
+
+	}
+}
--- tabbie2.git/frontend/views/site/about.php
+++ tabbie2.git/frontend/views/site/about.php
@@ -8,7 +8,44 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
 	<h1><?= Html::encode($this->title) ?></h1>
 
-	<p>This is the About page. You may modify the following file to customize its content:</p>
+	<p>Tabbie 2 is the new edition of the famous Tabbie system. Lots of cool new features and awesome stuff! Go check it
+		out.<br>
+		Yea I mean seriously, what else should I write here ... if you know something send me an email to
+		jakob@tabbie.org...
+	</p>
 
-	<code><?= __FILE__ ?></code>
+	<div class="row">
+		<div class="col-sm-6">
+			<h2>Import Data Masks</h2>
+			<?
+			$sampleDir = "@frontend/assets/csv/";
+			$asset = Yii::$app->assetManager->publish($sampleDir)[1];
+			?>
+			<ul>
+				<li><?= Html::a(Yii::t("app", "Venue CSV"), $asset . "/Import_Venue.csv") ?></li>
+				<li><?= Html::a(Yii::t("app", "Adjudicator CSV"), $asset . "/Import_Adjudicator.csv") ?></li>
+				<li><?= Html::a(Yii::t("app", "Team CSV"), $asset . "/Import_Team.csv") ?></li>
+			</ul>
+		</div>
+		<div class="col-sm-6">
+			<h2>Sample Data</h2>
+			<?
+			$sampleFile = Yii::getAlias($sampleDir) . "/Sample_Venues.csv";
+			if (file_exists($sampleFile)) {
+				$mtime = filemtime($sampleFile);
+				?>
+				<small><?= Yii::t("app", "created") ?> <?= Yii::$app->formatter->asDatetime($mtime) ?></small>
+				<br><br>
+				<ul>
+					<li><?= Html::a(Yii::t("app", "Sample Venue CSV"), $asset . "/Sample_Venues.csv") ?></li>
+					<li><?= Html::a(Yii::t("app", "Sample Team CSV"), $asset . "/Sample_Teams.csv") ?></li>
+					<li><?= Html::a(Yii::t("app", "Sample Adjudicator CSV"), $asset . "/Sample_Adjudicator.csv") ?></li>
+				</ul>
+			<?
+			}
+			else
+				echo "not yet created";
+			?>
+		</div>
+	</div>
 </div>
