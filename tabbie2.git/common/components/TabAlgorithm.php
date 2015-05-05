442bf31b5105ddb1c8da84041cfcdce8a428d530
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-05-05 10:31:20 +0200

    Additional Comments

--- tabbie2.git/common/components/TabAlgorithm.php
+++ tabbie2.git/common/components/TabAlgorithm.php
@@ -31,24 +31,42 @@ abstract class TabAlgorithm extends \yii\base\Object {
 	 */
 	public $round_number;
 
+	/**
+	 * Average Adjudicator Strength in the tournament
+	 *
+	 * @var float
+	 */
 	public $average_adjudicator_strength;
 
+	/**
+	 * Standard diviation of the Adjudicator Strength
+	 * @var float
+	 */
 	public $SD_of_adjudicators;
 
+	/**
+	 * Setter Class for the Draw
+	 * @see Round::improveAdjudicator
+	 * @param $draw
+	 */
 	public function setDraw($draw) {
 		$this->DRAW = $draw;
 	}
 
 	/**
 	 * Function that calculated the Draw for a round
+
 	 *
-	 * @param \common\models\Venue[]       $venues       Array of all active venues
+*@param \common\models\Venue[]       $venues       Array of all active venues
 	 * @param \common\models\Team[]        $teams        Array of all teams in the tournament in the structur of
 	 *                                                   array(
 	 *                                                   name,
 	 *                                                   institution,
 	 *                                                   )
 	 * @param \common\models\Adjudicator[] $adjudicators Array of all active adujicators
+	 * @param array                        $preset_panels
+	 *
+	 * @return
 	 */
 	abstract public function makeDraw($venues, $teams, $adjudicators, $preset_panels = array());
 
