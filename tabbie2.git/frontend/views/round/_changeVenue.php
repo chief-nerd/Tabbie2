b01583d0c11285e8d203872639d009fd496a86be
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-05-16 10:47:55 +0200

    Change Back to "change" trigger

--- tabbie2.git/frontend/views/round/_changeVenue.php
+++ tabbie2.git/frontend/views/round/_changeVenue.php
@@ -22,7 +22,7 @@ echo Select2::widget([
 	'data' => $venueOptions,
 	'options' => ['placeholder' => Yii::t("app", 'Select a Venue ...')],
 	"pluginEvents" => [
-		"select2:close" => "function() { document.getElementById('changeVenueForm').submit(); }",
+		"change" => "function() { document.getElementById('changeVenueForm').submit(); }",
 	]
 ]);
 $form->end();
