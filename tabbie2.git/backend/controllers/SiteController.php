0f64fc8c4cf68eb4ae761b13bd5f3a5c0a5f80fb
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-05-20 17:22:06 +0200

    Clean Backend index Page

--- tabbie2.git/backend/controllers/SiteController.php
+++ tabbie2.git/backend/controllers/SiteController.php
@@ -79,7 +79,6 @@ class SiteController extends Controller {
 
 	public function actionLogout() {
 		Yii::$app->user->logout();
-
 		return $this->goHome();
 	}
 
--- tabbie2.git/backend/views/site/index.php
+++ tabbie2.git/backend/views/site/index.php
@@ -10,39 +10,10 @@ $this->title = 'Welcome Tab Master';
     <div class="jumbotron">
 	    <h1>Welcome Tabbie2-Master!</h1>
 
-        <p class="lead">Are you ready? Wanna tab a tournament like-a pro?</p>
-
-        <p><?= Html::a("Create new tournament", ['tournament/create'], ["class" => "btn btn-lg btn-success"]) ?></p>
+	    <p class="lead">Welcome to the Backend</p>
     </div>
 
     <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Selling Point 1</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Selling Point 2</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Selling Point 3</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-            </div>
-        </div>
 
     </div>
 </div>
