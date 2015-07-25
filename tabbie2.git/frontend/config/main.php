b7cf12dbd3ec69b50f0ec68528f783897de37609
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-07-25 23:14:03 +0200

    remove column selector

--- tabbie2.git/frontend/config/main.php
+++ tabbie2.git/frontend/config/main.php
@@ -61,14 +61,6 @@ $config = [
 			'class' => 'yii\gii\Module', //adding gii module
 			'allowedIPs' => ['127.0.0.1', '::1']  //allowing ip's
 		],
-		'gridview' => [
-			'class' => '\kartik\grid\Module'
-			// enter optional module parameters below - only if you need to
-			// use your own export download action or custom translation
-			// message source
-			// 'downloadAction' => 'gridview/export/download',
-			// 'i18n' => []
-		]
 	],
 	'params' => $params,
 ];
