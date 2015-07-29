7c34cf9c6aee081f880e0bb05eb9cfa57eef457c
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-07-29 23:42:20 +0200

    Deactivate pjax for runner view

--- tabbie2.git/frontend/views/public/_venue.php
+++ tabbie2.git/frontend/views/public/_venue.php
@@ -6,5 +6,5 @@
 <?= \kartik\helpers\Html::a(
 	'<div class="status ' . (($model->result) ? "entered" : "missing") . '"></div>' . $model->venue->name,
 	["result/create", "tournament_id" => $model->tournament_id, "id" => $model->id],
-	["class" => "btn btn-default"]
+	["class" => "btn btn-default", "data-pjax" => 0]
 ) ?>
