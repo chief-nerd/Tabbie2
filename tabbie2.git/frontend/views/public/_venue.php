4bb3a1b8cfd94f72432c9fcb1811e2012bd01b35
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-07-31 17:26:11 +0200

    accessCreate

--- tabbie2.git/frontend/views/public/_venue.php
+++ tabbie2.git/frontend/views/public/_venue.php
@@ -5,6 +5,6 @@
 ?>
 <?= \kartik\helpers\Html::a(
 	'<div class="status ' . (($model->result) ? "entered" : "missing") . '"></div>' . $model->venue->name,
-	["result/create", "tournament_id" => $model->tournament_id, "id" => $model->id],
+	["result/create", "tournament_id" => $model->tournament_id, "id" => $model->id, "accessToken" => $model->tournament->accessToken],
 	["class" => "btn btn-default", "data-pjax" => 0]
 ) ?>
