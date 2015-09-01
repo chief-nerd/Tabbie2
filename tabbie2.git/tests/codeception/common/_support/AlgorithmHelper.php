702d7ad2b2c5676e2d5bcc4ea819694d8570b767
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-09-01 15:10:58 +0200

    Add Helper

---
+++ tabbie2.git/tests/codeception/common/_support/AlgorithmHelper.php
@@ -0,0 +1,10 @@
+<?php
+namespace tests\codeception\common\Codeception\Module;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class AlgorithmHelper extends \Codeception\Module
+{
+
+}
