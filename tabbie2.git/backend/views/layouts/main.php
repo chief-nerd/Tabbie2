0b29f2628a12d5a53ce4e79bbdeefc3341457536
Jakob Reiter <jakob.reiter.io@gmail.com>
2015-09-06 23:55:35 +0200

    Readd main layout

---
+++ tabbie2.git/backend/views/layouts/main.php
@@ -0,0 +1,80 @@
+<?php
+
+use backend\assets\AppAsset;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+	<!DOCTYPE html>
+	<html lang="<?= Yii::$app->language ?>">
+	<head>
+		<meta charset="<?= Yii::$app->charset ?>"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?= Html::csrfMetaTags() ?>
+		<title><?= Html::encode($this->title) ?>
+			:: <?= Html::encode(Yii::$app->params["appName"] . " " . Yii::t("app", "API")) ?></title>
+		<?php $this->head() ?>
+	</head>
+	<body>
+	<?php $this->beginBody() ?>
+	<div class="wrap">
+		<?php
+		NavBar::begin([
+			'brandLabel' => Yii::$app->params["appName"] . " " . Yii::t("app", "Master"),
+			'brandUrl' => Yii::$app->homeUrl,
+			'options' => [
+				'class' => 'navbar-inverse navbar-fixed-top',
+			],
+		]);
+		$menuItems = [
+			['label' => 'Home', 'url' => ['/site/index']],
+			['label' => 'Motion Tags', 'url' => ['motiontag/index']],
+			['label' => 'Societies', 'url' => ['society/index']],
+			['label' => 'Special Needs', 'url' => ['specialneeds/index']],
+		];
+		if (Yii::$app->user->isGuest) {
+			$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+		} else {
+			$menuItems[] = [
+				'label' => 'Logout (' . Yii::$app->user->identity->givenname . ')',
+				'url' => ['/site/logout'],
+				'linkOptions' => ['data-method' => 'post']
+			];
+		}
+		echo Nav::widget([
+			'options' => ['class' => 'navbar-nav navbar-right'],
+			'items' => $menuItems,
+		]);
+		NavBar::end();
+		?>
+
+		<div class="container">
+			<?=
+			Breadcrumbs::widget([
+				'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+			])
+			?>
+
+			<?= $content ?>
+		</div>
+	</div>
+
+	<footer class="footer">
+		<div class="container">
+			<p class="pull-left">&copy; Tabbie2 <?= date('Y') ?></p>
+
+			<p class="pull-right"><?= Yii::powered() ?></p>
+		</div>
+	</footer>
+
+	<?php $this->endBody() ?>
+	</body>
+	</html>
+<?php $this->endPage() ?>
