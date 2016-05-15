985a2ce7209e4bfd902baeb3e2c6c44f77cec1c7
JakobReiter <jakob.reiter.io@gmail.com>
2016-05-15 10:56:15 +0200

    Prep for open source, Readme1 ignore and clean

--- tabbie2.git/environments/prod/common/config/main-local.php
+++ tabbie2.git/environments/prod/common/config/main-local.php
@@ -4,18 +4,18 @@ return [
 	'components' => [
 		'db' => [
 			'class' => 'yii\db\Connection',
-			'dsn' => 'mysql:host=tabbiedb3.cnzwcwadwjrx.eu-central-1.rds.amazonaws.com;dbname=tabbie',
-			'username' => 'tabbieRoot',
-			'password' => '8ooHLD7XZx777G5AtT66',
+			'dsn' => 'mysql:host=127.0.0.1;dbname=tabbie',
+			'username' => 'tabbie',
+			'password' => '',
 			'enableSchemaCache' => true,
 		],
 		'mailer' => [
 			'useFileTransport' => false,
 			'transport' => [
 				'class' => 'Swift_SmtpTransport',
-				'host' => 'smtp.mandrillapp.com',
-				'username' => 'admin@tabbie.org',
-				'password' => 'v8yVwjk2BBk7UvNdkq5BaQ',
+				'host' => '',
+				'username' => '',
+				'password' => '',
 				'port' => '587',
 			],
 		],
