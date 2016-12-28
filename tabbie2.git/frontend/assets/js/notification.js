12825ecad550f1f1264ac7201e0e1487c8c737fe
rscoates <prefecttophat@gmail.com>
2016-12-28 22:57:30 +0100

    Notification update.

--- tabbie2.git/frontend/assets/js/notification.js
+++ tabbie2.git/frontend/assets/js/notification.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var socket = io.connect('http://192.168.1.80:8890');
+    var socket = io.connect('http://192.168.178.45:8890');
 
     socket.on('notification', function (data) {
 
